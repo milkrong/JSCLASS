63d1aff27e93acf365f77be5f7da53c7d872df21
milkrong <milkrong121@outlook.com>
2019-12-10 10:46:05 -0800

    reactModal Homework

---
+++ homeworkModal/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Modal from './modal';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    }
+  }
+
+  showModal = () => {
+    this.setState({show:true});
+  };
+
+  closeModal = () => {
+    this.setState({show:false})
+  }
+
+  render() {
+    const content = <p>Pellentesque habitant morbi tristique senectus et netus et malesuada 
+      fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
+      tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+      Aenean ultricies mi vitae est. Mauris placerat eleifend leo. 
+      Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, 
+      condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum</p>;
+    return (
+      <div className="App">
+        <button 
+          className="toggle-button"
+          onClick={this.showModal}>
+          Toggle
+        </button>
+        <Modal
+          show={this.state.show}
+          handleClose={this.closeModal}
+          buttonText="Open"
+          cancelButtonText="Go Back"
+          content={content}
+          width={400}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ homeworkModal/src/modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './modal.css'
+
+const Modal = (props) => {
+    let modalClass = props.show? "modal open" : "modal"
+    return (
+        <div className={modalClass}>
+            <div className="wrapper" style={{width : props.width}}>
+              <div className="header">
+              Title
+              <i class="fas fa-times" onClick={props.handleClose}></i>
+              </div>
+              <div className="content">
+                {props.content}
+              </div>
+              <div className="footer">
+              <button style={{backgroundColor: 'grey'}} onClick={props.handleClose} >{props.cancelButtonText}</button>
+                <button>{props.buttonText}</button>
+              </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
